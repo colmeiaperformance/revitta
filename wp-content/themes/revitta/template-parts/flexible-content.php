2155c90963401ef6ba0f34303b7147d5292ed24c
brunarafaela <brunarafaelav@outlook.com>
2022-01-26 12:16:35 -0300

    Add imagens na header

--- wp-content/themes/revitta/template-parts/flexible-content.php
+++ wp-content/themes/revitta/template-parts/flexible-content.php
@@ -9,8 +9,6 @@
 
       <div class="carousel-item">
         <img class="bd-placeholder-img" src="<?php echo get_template_directory_uri() . '/images/header.svg' ?>" alt="Logo">
-        <!-- <svg class="bd-placeholder-img" width="100%" height="100vh" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" 
-        preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#20585E"></rect></svg> -->
         <div class="container">
           <div class="carousel-caption text-center">
             <h1>We're here to help you find your most precious gift: yourself!</h1>
@@ -21,7 +19,7 @@
       </div>
 
       <div class="carousel-item active">
-        <svg class="bd-placeholder-img" width="100%" height="100vh" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#20585E"></rect></svg>
+        <img class="bd-placeholder-img" src="<?php echo get_template_directory_uri() . '/images/header.svg' ?>" alt="Logo">
         <div class="container">
           <div class="carousel-caption text-center">
             <h1>We're here to help you find your most precious gift: yourself!</h1>
@@ -32,7 +30,7 @@
       </div>
 
       <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100vh" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#20585E"></rect></svg>
+        <img class="bd-placeholder-img" src="<?php echo get_template_directory_uri() . '/images/header.svg' ?>" alt="Logo">
         <div class="container">
           <div class="carousel-caption text-center">
             <h1>We're here to help you find your most precious gift: yourself!</h1>
