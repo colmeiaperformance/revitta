e843b82907ca47ca26bf72643dc439e18721c448
brunarafaela <brunarafaelav@outlook.com>
2022-02-01 23:39:28 -0300

    Add form style

--- wp-content/themes/revitta/template-parts/flexible-content.php
+++ wp-content/themes/revitta/template-parts/flexible-content.php
@@ -314,102 +314,103 @@
   </div>
 </section>
 
-<section class="consultation">
-  <div class="container"> 
-    <h2 class="text-center">Customized consultation</h2>
-    <p class="text-center">Reserve your customized consultation</p>
-    <form class="needs-validation" novalidate>
+<section class="customized-consultation">
+  <div class="container pt-lg-5 pb-5"> 
+    <h2 class="pt-4 pb-3 mt-1 text-uppercase text-center">Customized consultation</h2>
+    <p class="text-black text-center my-0 mx-auto">Reserve your customized consultation</p>
+    <form class="needs-validation mt-5 pt-3" novalidate>
       <div class="row">
-        <div class=col-md-6>
-           <div class="mb-3">
+        <div class="col-md-6 pe-lg-4">
+           <div class="mb-4">
             <input type="text" class="form-control" id="inputName" placeholder="Full name" required>
           </div>
-          <div class="mb-3">
+          <div class="mb-4">
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" required>
           </div>
-          <div class="mb-3">
+          <div class="mb-4">
             <input type="tel" id="phone" class="form-control" placeholder="Phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
           </div>
-          <div class="mb-3">
-            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message" rows="3"></textarea>
+          <div class="mb-4">
+            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message" rows="5"></textarea>
           </div>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 ps-lg-5">
+          <h3 class="text-dark text-center">All i want is...</h3>
           <div class="row">
             <div class="col-6">
               <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1" checked>
+            <label class="form-check-label" for="flexCheckDefault1">
+              Tired to shave
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked2">
+            <label class="form-check-label" for="flexCheckChecked2">
+              Facial Volume and fullness
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault3">
+            <label class="form-check-label" for="flexCheckDefault3">
+              New, regenerated skin
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked4">
+            <label class="form-check-label" for="flexCheckChecked4">
+              Boost your skin
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault5">
+            <label class="form-check-label" for="flexCheckDefault5">
+              Feel younger inside
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked6" checked>
+            <label class="form-check-label" for="flexCheckChecked6">
+             The magic of PRP!
             </label>
           </div>
             </div>
 
             <div class="col-6">
               <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault7">
+            <label class="form-check-label" for="flexCheckDefault7">
+               Youth-ful looking skin
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked8">
+            <label class="form-check-label" for="flexCheckChecked8">
+              Smooth and soften wrinkles
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault9">
+            <label class="form-check-label" for="flexCheckDefault9">
+              Get in shape
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked10">
+            <label class="form-check-label" for="flexCheckChecked10">
+              Great hair, we care
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-              Default checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault11">
+            <label class="form-check-label" for="flexCheckDefault11">
+              It’s in your blood
             </label>
           </div>
           <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-            <label class="form-check-label" for="flexCheckChecked">
-              Checked checkbox
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked12">
+            <label class="form-check-label" for="flexCheckChecked12">
+              Back to enjoyng life
             </label>
           </div>
 
@@ -419,7 +420,9 @@
 
         </div>
       </div>
-      <button type="submit" class="btn btn-primary">Book now</button>
+      <div class="mt-3 text-center">
+        <button type="submit" class="btn text-uppercase">Book now</button>
+      </div>
     </form>
   </div>
 </section>
