6197c4c90577954277e93ea0b5a14d2467959942
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-02-07 18:09:04 -0300

    Update functions.php svg img upload

--- wp-content/themes/revitta/functions.php
+++ wp-content/themes/revitta/functions.php
@@ -277,7 +277,7 @@ function cc_mime_types( $mimes ){
   $mimes['svg'] = 'image/svg+xml';
   return $mimes;
 }
-add_filter( 'upload_mimes', 'cc_mime_types' );
+add_filter( 'upload_mimes', 'my_custom_mime_types' );
 
 function fix_svg() {
   echo '<style type="text/css">
