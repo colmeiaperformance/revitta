1d3f5675d6e2031e8e2a661c6e08a669f311edb7
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-02-07 18:43:26 -0300

    Updated some plugins and functions svg removed
    Add svg support, updated some plugins, removing svg function from functions.php file, but kept commented.

--- wp-content/themes/revitta/functions.php
+++ wp-content/themes/revitta/functions.php
@@ -256,28 +256,28 @@ add_theme_support( 'custom-spacing' );
 add_action( 'after_setup_theme', 'base_setup' );
 
 // Allow SVG
-add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+// add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
 
-  global $wp_version;
-  if ( $wp_version !== '4.7.1' ) {
-     return $data;
-  }
+//   global $wp_version;
+//   if ( $wp_version !== '4.7.1' ) {
+//      return $data;
+//   }
 
-  $filetype = wp_check_filetype( $filename, $mimes );
+//   $filetype = wp_check_filetype( $filename, $mimes );
 
-  return [
-      'ext'             => $filetype['ext'],
-      'type'            => $filetype['type'],
-      'proper_filename' => $data['proper_filename']
-  ];
+//   return [
+//       'ext'             => $filetype['ext'],
+//       'type'            => $filetype['type'],
+//       'proper_filename' => $data['proper_filename']
+//   ];
 
-}, 10, 4 );
+// }, 10, 4 );
 
-function cc_mime_types( $mimes ){
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter( 'upload_mimes', 'my_custom_mime_types' );
+// function cc_mime_types( $mimes ){
+//   $mimes['svg'] = 'image/svg+xml';
+//   return $mimes;
+// }
+// add_filter( 'upload_mimes', 'my_custom_mime_types' );
 
 function fix_svg() {
   echo '<style type="text/css">
