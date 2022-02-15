e3656c4c23d2da301fe29b57601dc657aed62774
Igor Fonseca <igor.stutzf@gmail.com>
2022-02-15 11:36:40 -0300

    update sec-about-us, logo home, plugins

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1645540300;s:7:"authKey";s:64:"aA)^7{(9D,v/vOUO<h(birN7;9}!yAU-9q[n[OzG7G~!>7e:z>[O)8p=?}+D-8B=";s:7:"version";s:5:"1.0.4";}
